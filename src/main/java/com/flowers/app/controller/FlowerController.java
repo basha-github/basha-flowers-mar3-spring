db38c826986e5047575873bc1111f9b0028a585f
sharifabee <sharifabee.alur@gmail.com>
2025-03-03 10:51:16 +0530

    url added

--- src/main/java/com/flowers/app/controller/FlowerController.java
+++ src/main/java/com/flowers/app/controller/FlowerController.java
@@ -22,32 +22,85 @@ public class FlowerController {
 	@Autowired
 	FlowerService flSer;
 	
+	//http://localhost:8080/flowers/app/add/new
+	//Post
+	/*
+	 * 
+	 * {
+
+    "name":"Rose",
+    "category":"fragarance",
+    "price":500,
+    "imgUrl":"http://ruyrur"
+}
+	 * 
+	 */
 	
 	@PostMapping("/flowers/app/add/new")
 	public String addNewFlower(@RequestBody Flower flower) {
 		return flSer.saveNewFlower(flower);
 	}
 	
-
+// http://localhost:8080/flowers/app/add/all
+	/*
+	 * [
+    {
+        "name": "ABC",
+        "category": "fragarance",
+        "price": 600,
+        "imgUrl": "http://ruyrur"
+    },
+    {
+        "name": "BCD",
+        "category": "fragarance",
+        "price": 800,
+        "imgUrl": "http://ruyrur"
+    },
+    {
+        "name": "XYZ",
+        "category": "fragarance",
+        "price": 600,
+        "imgUrl": "http://ruyrur"
+    }
+]
+	 */
 	@PostMapping("/flowers/app/add/all")
 	public String addAllFlowers(@RequestBody List<Flower> flowerList) {
 		return flSer.saveAllFlowers(flowerList);
 	}
+	
+	//http://localhost:8080/flowers/app/get/all
 	
 	@GetMapping("/flowers/app/get/all")
 	public List<Flower> getAllFlowers() {
 		return flSer.getAllFlowers();
 	}
 	
+	//http://localhost:8080/flowers/app/get/one?flowerName=XYZ
 	@GetMapping("/flowers/app/get/one")
 	public Flower getOneFlower(@RequestParam String flowerName) {
 		return flSer.getFlower(flowerName);
 	}
 	
+	//http://localhost:8080/flowers/app/update/one
+	//method--->PUT
+	/*
+	 * {
+    "name": "XYZ",
+    "category": "Decorative",
+    "imgUrl": "http://jasmine",
+    "price": 1200
+}
+	 * 
+	 * 
+	 */
 	@PutMapping("/flowers/app/update/one")
 	public String updateFlower(@RequestBody Flower flower) {
 		return flSer.updateFlower(flower);
 	}
+	
+	//http://localhost:8080/flowers/app/delete/one?flowerName=ABC
+//method--->DELETE
 	
 	@DeleteMapping("/flowers/app/delete/one")
 	public String deleteOneFlower(@RequestParam String flowerName) {
